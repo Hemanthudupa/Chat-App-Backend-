7560a9c96b4907be2430c59d6aef8086908b914b
Hemanthudupa <hemanthudupa06@gmail.com>
2025-04-01 13:06:02 +0530

    configure database connection using environment variables and update launch configuration

--- database.ts
+++ database.ts
@@ -1,19 +1,19 @@
 import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize({
-  database: "chat-app",
+  database: process.env.DB_NAME,
   dialect: "postgres",
   host: "localhost",
-  password: "root",
+  password: process.env.DB_PASS,
   pool: {
     max: 6,
     min: 0,
     idle: 5000,
   },
-  username: "postgres",
-  port: 5433,
-});
+  username: process.env.DB_USER,
+  port: process.env.DB_PORT,
+} as any);
 export async function createConnection() {
   await sequelize.authenticate();
   console.log(" successfully connected to data base");
-  return "hello"
+  return "hello";
 }
